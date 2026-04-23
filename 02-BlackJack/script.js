ab431d5984aa3338a4d261a8b60357b573f623b3
Teniola Awodire <tenioladev@gmail.com>
2026-04-23 13:31:21 +0100

    BlackJack Learning

---
+++ 02-BlackJack/script.js
@@ -0,0 +1,15 @@
+let firstCard = 4
+let secondCard = 5
+
+let sum = firstCard + secondCard 
+
+// IF Else Class-work
+
+let age = 22
+
+if(age < 21) {
+console.log("You cannot enter the club")
+}
+else {
+console.log("Welcome")
+}
