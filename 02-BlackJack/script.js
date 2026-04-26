212f9cb10bd590eb8c6cddef0d72bdf0bdddaae0
Teniola Awodire <tenioladev@gmail.com>
2026-04-26 17:58:27 +0100

    Refactor Blackjack logic to improve readability and structure

--- 02-BlackJack/script.js
+++ 02-BlackJack/script.js
@@ -1,15 +1,22 @@
 let firstCard = 4
 let secondCard = 5
-
 let sum = firstCard + secondCard 
+let hasBlackJack = false
+let isAlive = true
 
 if(sum <= 20) {
 console.log("Do you want to draw a new card? 🙂")
 }
 else if(sum === 21) {
 console.log("Wohoo! You've got Blackjack! 🥳")
-} else {
-   console.log("You're out of the game! 😭")
+hasBlackJack = true 
 }
+else {
+   console.log("You're out of the game! 😭")
+   isAlive = false
+}
+
+console.log(isAlive);
+
  
  
