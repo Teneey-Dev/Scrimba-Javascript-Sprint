2d8c37ff78a6b655a4dd455f8b8693f0b2f7546b
Teniola Awodire <tenioladev@gmail.com>
2026-05-24 17:57:50 +0100

    learnt return

--- 02-BlackJack/script.js
+++ 02-BlackJack/script.js
@@ -1,24 +1,35 @@
-let firstCard = 10
-let secondCard = 5
+let firstCard = getRandomcard()
+let secondCard = getRandomcard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
-let message = ""
+let message = "" 
+let sumEl = document.querySelector("#sum-el")
+let messageEl = document.querySelector("#message-el")
+let cardsEL = document.querySelector("#card-el")
+
+function getRandomcard() {
+ return 5
+   // return random
+}
+
+
+
 
 function startGame() {
    renderGame()
 }  
 
- 
-
-
-let sumEl = document.querySelector("#sum-el")
-let messageEl = document.querySelector("#message-el")
-let cardsEL = document.querySelector("#card-el")
 
 function renderGame () {
-   cardsEL.textContent = "Cards: " + cards[0] + " " + cards[1] 
+   cardsEL.textContent = "Cards: " 
+   for (let i = 0; i < cards.length; i++) {
+       cardsEL.textContent += cards[i] + " ";
+   }
+
+
+
    sumEl.textContent = "Sum:  " + sum
 if(sum <= 20) {
 message = "Do you want to draw a new card? "
@@ -37,7 +48,7 @@ messageEl.textContent = message
  
 function newCard() {
    console.log("Drawing a new card from the deck")
-let card = 4
+let card = getRandomcard()
 sum += card
 cards.push(card)
 renderGame()
