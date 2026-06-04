b098f20ab2cb1fc53de2e5dbbd68993bfdc481d7
Teniola Awodire <tenioladev@gmail.com>
2026-06-04 03:39:09 +0100

    Javascript Update

---
+++ 03-Chrome Extension/script.js
@@ -0,0 +1,8 @@
+
+let myLeads = []
+const inputEl = document.querySelector("#input-el")
+const inputBtn = document.querySelector("#input-btn")
+
+inputBtn.addEventListener ("click", function () {
+    console.log("Button clicked")
+})
