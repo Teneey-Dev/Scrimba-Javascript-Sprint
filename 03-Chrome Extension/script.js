ce509688532dbed0cef0ed2e80119641755283fd
Teniola Awodire <tenioladev@gmail.com>
2026-06-04 13:36:47 +0100

    Initialize myLeads array and update input handling for lead storage

--- 03-Chrome Extension/script.js
+++ 03-Chrome Extension/script.js
@@ -1,8 +1,19 @@
 
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
+let inputText = inputEl.value
+const ulEl = document.querySelector("#ul-el")
+
+console.log(ulEl)
 
 inputBtn.addEventListener ("click", function () {
-    console.log("Button clicked")
-})
+
+myLeads.push(inputText)
+ console.log(myLeads)
+
+})  
+
+for (let i = 0; i < myLeads.length; i++) {
+   ulEl.innerHTML += "<li>" +  myLeads[i] + " " + "</li>"
+} 
